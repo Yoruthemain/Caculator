143acf20b0b81a923444a6e7f6c6c098ce81e966
Yoruthemain <108297117+Yoruthemain@users.noreply.github.com>
2024-06-27 13:18:39 -0400

    upd

--- caculator.js
+++ caculator.js
@@ -7,6 +7,7 @@ console.log(allclearbtn);
 let equalsbtn=document.querySelector("[data-equals]")
 let previous_Operand=document.querySelector("[data-previous-operand")
 let current_Operand=document.querySelector("[data-current-operand]")
+let Deletebtn=document.querySelector("[data-delete]")
 class Caculator{
     constructor(previous_Operand_Element,current_Operand_Element){
         this.previous_Operand_Element=previous_Operand_Element
@@ -17,6 +18,44 @@ append_Number(number){
     
     this.current_Operand = this.current_Operand.toString()+number.toString()
     // console.log(this.current_Operand);
+}
+Delete(){
+    this.current_Operand=this.current_Operand.toString().slice(0,-1)
+}
+Choose_Operation(operation){
+    if (this.current_Operand==="") return
+    if (this.previous_Operand!=""){
+        this.compute()
+    }
+    this.operation=operation
+    this.previous_Operand=this.current_Operand
+    this.current_Operand=""
+}
+compute(){
+    let compute
+    let previous=parseFloat(this.previous_Operand)
+    let current=parseFloat(this.current_Operand)
+    if (isNaN(previous)||isNaN(current)) return
+    switch (this.operation) {
+        case '+':
+        compute=previous+current
+       
+            break;
+        case "-":
+            compute=previous-current
+            break;
+        case "÷":
+            compute=previous/current
+        break;
+        case "×":
+            compute=previous*current
+            break;
+        default:
+            return;
+    }
+    this.current_Operand=compute
+    this.operation=undefined
+    this.previous_Operand=""
 }
 clear(){
     this.previous_Operand=""
@@ -36,3 +75,13 @@ numbersbtn.forEach(button=>{
         caculator.update_Display()
     });
 })
+Deletebtn.addEventListener("click",()=>{
+    caculator.Delete()
+    caculator.update_Display()
+})
+operationbtn.forEach(element => {
+    element.addEventListener('click', ()=>{
+        caculator.Choose_Operation(element.innerText)
+        caculator.update_Display()
+    })
+});
